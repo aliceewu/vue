3f2bd6e02e11b7f3e42b9c7436417170507bb1bc
ZB1018144 <1002186944@qq.com>
2020-06-19 23:18:28 +0800

    init my project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="app-container">
+		<!-- 顶部Header区域 -->
+		<mt-header fixed title="vue商城"></mt-header>
+		<!-- 中间的路由router-view 区域 -->
+		
+		<!-- 底部Tabbar 区域 -->
+		<nav class="mui-bar mui-bar-tab">
+					<a class="mui-tab-item mui-active" href="#tabbar">
+						<span class="mui-icon mui-icon-home"></span>
+						<span class="mui-tab-label">首页</span>
+					</a>
+					<a class="mui-tab-item" href="#tabbar-with-contact">
+						<span class="mui-icon mui-icon-contact"></span>
+						<span class="mui-tab-label">会员</span>
+					</a>
+					<a class="mui-tab-item" href="#tabbar-with-chat">
+						<span class="mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">9</span></span>
+						<span class="mui-tab-label">购物车</span>
+					</a>
+					
+					<a class="mui-tab-item" href="#tabbar-with-map">
+						<span class="mui-icon mui-icon-search"></span>
+						<span class="mui-tab-label">搜索</span>
+					</a>
+				</nav>
+		<h1>123</h1>
+	</div>
+</template>
+
+<script>
+	
+</script>
+<style lang="scss" scoped>
+.app-container{
+	padding-top: 40px;
+}	
+</style>
